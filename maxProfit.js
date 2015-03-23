90bae7efdccb913f4a46918910255b9dc46887dd
Zoila Ramirez <zoila@outlook.com>
2015-03-23 14:39:10 -0700

    Update maxProfit.js

--- maxProfit.js
+++ maxProfit.js
@@ -1,12 +1,12 @@
 /*
- * input: positive integers array
- * output: Max profit
- */
+* input: positive integers array
+* output: Max profit
+*/
 function getMaxProfit(houses) {
-	if (!houses) {
-		return 0;
-	}
-	
+if (!houses) {
+	return 0;
+}
+
 	var houseCon = function(arr) {
 		var costRobFirst;
 		var costRobSecond;
@@ -19,7 +19,7 @@ function getMaxProfit(houses) {
 			costRobFirst = (i + 1 < arr.length) ? arr[i + 1] : 0;
 			costRobSecond = (i + 2 < arr.length) ? arr[i + 2] + arr[i] : arr[i];
 		
-			if (costRobFirst < costRobSecond - correction) {
+			if (costRobFirst <= costRobSecond - correction) {
 				bestRob += arr[i];
 				i += 2;
 				shortJump = true;
@@ -35,24 +35,3 @@ function getMaxProfit(houses) {
 	
 	return "$" + houseCon(houses);
 }
-
-
-function testHouseCon(arr) {
-	console.log("[" + arr +"] => optimal robbing "+ getMaxProfit(arr));
-	
-}
-
-testHouseCon([20, 10, 50, 5, 1]);
-testHouseCon([20, 50, 10, 1, 5]);
-testHouseCon([200]);
-testHouseCon([200, 200]);
-testHouseCon([200, 200, 200]);
-testHouseCon([200, 399, 200]);
-testHouseCon([200, 399, 200, 2]);
-testHouseCon([200, 400, 200]);
-testHouseCon([200, 401, 200]);
-testHouseCon([]);
-testHouseCon();
-testHouseCon([20, 50, 10, 1, 5, 20, 50, 10, 1, 5]);
-testHouseCon([20, 50, 10, 1, 50, 20, 20, 50, 10]);
-testHouseCon([20, 50, 10, 1, 50, 20, 50, 20, 10]);	
